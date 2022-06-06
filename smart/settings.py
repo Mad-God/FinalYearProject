c0b0bb535462122a2e680e23781e693bacc58d00
Satyam Singh <55124399+Mad-God@users.noreply.github.com>
2022-06-06 10:20:46 +0530

    Update settings.py

--- smart/settings.py
+++ smart/settings.py
@@ -159,14 +159,6 @@ CRISPY_TEMPLATE_PACK = 'tailwind'
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'tailwind'
 
 
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'stmsng2001@gmail.com'
-EMAIL_HOST_PASSWORD = 'nlcldzpaylwwptan'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-
 
 
 # for PHONENUMBERFIELD
